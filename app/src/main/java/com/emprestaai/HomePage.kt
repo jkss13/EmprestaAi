93ac78b1c356e38ee592684e946f22394daca692
Karine Santos <jkss13@discente.ifpe.edu.br>
2024-04-17 18:18:58 -0300

    Prática 02 – Parte 0 – Preparação e Ajustes

---
+++ app/src/main/java/com/emprestaai/HomePage.kt
@@ -0,0 +1,35 @@
+package com.emprestaai
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun HomePage(activity: Activity, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "Bem vindo(a)!",
+            fontSize = 24.sp
+        )
+        Button(
+            onClick = { activity?.finish() }
+        ) {
+            Text("Sair")
+        }
+    }
+    }
--- app/src/main/java/com/emprestaai/MainActivity.kt
+++ app/src/main/java/com/emprestaai/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.emprestaai
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.emprestaai.ui.theme.EmprestaAiTheme
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,23 +26,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun HomePage(activity: Activity, modifier: Modifier = Modifier) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "Bem vindo(a)!",
-                fontSize = 24.sp
-            )
-            Button(
-                onClick = { activity?.finish() }
-            ) {
-                Text("Sair")
-            }
-        }
-    }
