364c10ec9e552e56fc0fa19dc21c35ad707017b8
Karine Santos <jkss13@discente.ifpe.edu.br>
2024-04-18 17:52:32 -0300

    Prática 03 – Parte 3 – Adicionando elementos com um Diálogo

--- app/src/main/java/com/emprestaai/MainActivity.kt
+++ app/src/main/java/com/emprestaai/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.emprestaai.nav.BottomNavBar
@@ -29,7 +31,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val viewModel : MainViewModel by viewModels()
+            var showDialog = remember { mutableStateOf(false) }.value
             EmprestaAiTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -48,7 +57,7 @@ class MainActivity : ComponentActivity() {
                         BottomNavBar(navController = navController)
                     },
                     floatingActionButton = {
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(onClick = { showDialog = true }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
